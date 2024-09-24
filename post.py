766b7efa5ab9bf976b5105157a85e000aa1429c4
Arthur Schurhaus <arthurschur@icloud.com>
2024-09-24 15:21:38 -0300

    deletei e coloquei app numa folder.

--- post.py
+++
@@ -1,8 +0,0 @@
-class Post:
-    def __init__(self, conteudo: str, autor: Admin):
-        self.__conteudo = conteudo
-        self.__likes = []  # Um post pode ter muitos likes
-        self.__autor = autor
-
-    def receive_like(self, like):
-        self.likes.append(like)
--- topico.py
+++
@@ -1,4 +0,0 @@
-class Topico:
-    def __init__(self, name) -> None:
-        self.__posts = []
-        self.__name = ""
