166bf7bc72695eaf708bb7f5bd760107e42d48f1
Arthur Schurhaus <arthurschur@icloud.com>
2024-11-28 19:21:04 -0300

    implementado relatorio e telaUsuario.py

--- socialmedia/services/relatorio.py
+++ socialmedia/services/relatorio.py
@@ -1,10 +1,51 @@
+import PySimpleGUI as sg
+
+
 class Relatorio:
     def __init__(self, controle_post):
         self.__controle_post = controle_post
+        sg.theme('Reddit')
 
-    def post_mais_curtido(self):
+    def mostrar_relatorios(self):
+        # Get all report data first
+        post_curtido = self.__get_post_mais_curtido()
+        topico_posts = self.__get_topico_mais_posts()
+        topico_interacoes = self.__get_topico_mais_interacoes()
+        autor = self.__get_autor_mais_curtido()
+
+        layout = [
+            [sg.Text("Relatórios do Sistema", font=('Helvetica', 16, 'bold'),
+                     justification='center')],
+
+            # Post mais curtido
+            [sg.Frame("Post Mais Curtido", [
+                [sg.Text(self.__format_post_curtido(post_curtido), key='-POST-')]
+            ], font=('Helvetica', 12))],
+
+            # Tópico com mais posts
+            [sg.Frame("Tópico com Mais Posts", [
+                [sg.Text(self.__format_topico_posts(topico_posts), key='-TOPICO-')]
+            ], font=('Helvetica', 12))],
+
+            # Tópico com mais interações
+            [sg.Frame("Tópico com Mais Interações", [
+                [sg.Text(self.__format_topico_interacoes(topico_interacoes), key='-INTERACOES-')]
+            ], font=('Helvetica', 12))],
+
+            # Autor mais curtido
+            [sg.Frame("Autor Mais Curtido", [
+                [sg.Text(self.__format_autor_curtido(autor), key='-AUTOR-')]
+            ], font=('Helvetica', 12))],
+
+            [sg.Button('Fechar', key='-CLOSE-', size=(10, 1))]
+        ]
+
+        window = sg.Window('Relatórios', layout, modal=True)
+        event, _ = window.read()
+        window.close()
+
+    def __get_post_mais_curtido(self):
         if not self.__controle_post.posts:
-            print('Nenhum post cadastrado')
             return None
 
         post_mais_curtido = None
@@ -14,14 +55,12 @@ class Relatorio:
                 post_mais_curtido = post
                 maior_qtd_curtidas = len(post.likes)
 
-        if post_mais_curtido:
-            print(f'Post mais curtido: {post_mais_curtido.titulo} , com {maior_qtd_curtidas} curtidas. Feito por {post_mais_curtido.autor.username} \n')
-            return post_mais_curtido
+        return (post_mais_curtido, maior_qtd_curtidas) if post_mais_curtido else None
 
-    def topico_com_mais_posts(self):
+    def __get_topico_mais_posts(self):
         if not self.__controle_post.posts:
-            print('Nenhum post cadastrado')
             return None
+
         count_post_por_topico = {}
         for post in self.__controle_post.posts:
             if post.topico.nome not in count_post_por_topico:
@@ -30,24 +69,14 @@ class Relatorio:
                     count_post_por_topico[topico] += 1
                 else:
                     count_post_por_topico[topico] = 1
-        topico_mais_popular = None
-        maior_qtd_posts = -1
 
-        for topico, contagem in count_post_por_topico.items():
-            if contagem > maior_qtd_posts:
-                topico_mais_popular = topico
-                maior_qtd_posts = contagem
-
-        if topico_mais_popular:
-            print(f'Topico com mais posts: {topico_mais_popular.nome} , com {maior_qtd_posts} posts \n')
-            return topico_mais_popular
-        else:
-            print('Nenhum post cadastrado')
+        if not count_post_por_topico:
             return None
 
-    def topico_com_mais_interacoes(self):
+        return max(count_post_por_topico.items(), key=lambda x: x[1])
+
+    def __get_topico_mais_interacoes(self):
         if not self.__controle_post.posts:
-            print('Nenhum post cadastrado')
             return None
 
         interacoes_por_topico = {}
@@ -59,24 +88,13 @@ class Relatorio:
             else:
                 interacoes_por_topico[topico] = interacoes
 
-        topico_mais_interacoes = None
-        maior_qtd_interacoes = -1
-
-        for topico, interacoes in interacoes_por_topico.items():
-            if interacoes > maior_qtd_interacoes:
-                topico_mais_interacoes = topico
-                maior_qtd_interacoes = interacoes
-
-        if topico_mais_interacoes:
-            print(f'Tópico com mais interações: {topico_mais_interacoes.nome} , com {maior_qtd_interacoes} interações \n')
-            return topico_mais_interacoes
-        else:
-            print('Nenhum post cadastrado')
+        if not interacoes_por_topico:
             return None
 
-    def autor_mais_curtido(self):
+        return max(interacoes_por_topico.items(), key=lambda x: x[1])
+
+    def __get_autor_mais_curtido(self):
         if not self.__controle_post.posts:
-            print('Nenhum post cadastrado')
             return None
 
         curtidas_por_autor = {}
@@ -88,17 +106,45 @@ class Relatorio:
             else:
                 curtidas_por_autor[autor] = curtidas
 
-        autor_mais_curtido = None
-        maior_qtd_curtidas = -1
-
-        for autor, curtidas in curtidas_por_autor.items():
-            if curtidas > maior_qtd_curtidas:
-                autor_mais_curtido = autor
-                maior_qtd_curtidas = curtidas
-
-        if autor_mais_curtido:
-            print(f'Autor mais curtido: {autor_mais_curtido.username} , com {maior_qtd_curtidas} curtidas \n')
-            return autor_mais_curtido
-        else:
-            print('Nenhum post cadastrado')
+        if not curtidas_por_autor:
             return None
+
+        return max(curtidas_por_autor.items(), key=lambda x: x[1])
+
+    # Formatting methods
+    def __format_post_curtido(self, data):
+        if not data:
+            return "Nenhum post cadastrado"
+        post, curtidas = data
+        return f"Título: {post.titulo}\nCurtidas: {curtidas}\nAutor: {post.autor.username}"
+
+    def __format_topico_posts(self, data):
+        if not data:
+            return "Nenhum post cadastrado"
+        topico, quantidade = data
+        return f"Tópico: {topico.nome}\nQuantidade de Posts: {quantidade}"
+
+    def __format_topico_interacoes(self, data):
+        if not data:
+            return "Nenhum post cadastrado"
+        topico, interacoes = data
+        return f"Tópico: {topico.nome}\nTotal de Interações: {interacoes}"
+
+    def __format_autor_curtido(self, data):
+        if not data:
+            return "Nenhum post cadastrado"
+        autor, curtidas = data
+        return f"Username: {autor.username}\nTotal de Curtidas: {curtidas}"
+
+    # Original methods now just call the visualization
+    def post_mais_curtido(self):
+        self.mostrar_relatorios()
+
+    def topico_com_mais_posts(self):
+        self.mostrar_relatorios()
+
+    def topico_com_mais_interacoes(self):
+        self.mostrar_relatorios()
+
+    def autor_mais_curtido(self):
+        self.mostrar_relatorios()
